71489b2915e0e0e442f8c4b5a92828d16c15d4fe
Lincoln Ribeiro de Paula <lincoln20012@outlook.com>
2022-12-12 19:23:28 -0300

    v03

---
+++ Introdução_C#/Tipos_Dados/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int idade = 24; // valor inteiro sem casas decimais, pode assubir 2 bilhoes
+string name = "lincoln";  //cadeia de caracters
+bool maioridade = true; //valor booleano
+double idade2 = 24.2D; //valor numerico flutuante + utilizado
+float idade3 = 20.2F;
+decimal salario = 20.5M; //valor decimal geralmente usado para valores monetários
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+
+Console.WriteLine(dataAtual);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+Console.WriteLine(idade);
+Console.WriteLine(name);
+Console.WriteLine(maioridade);
+Console.WriteLine(idade2);
+Console.WriteLine(idade3);
+Console.WriteLine(salario);
+
+double altura = 1.80;
+
+Console.WriteLine(altura.ToString("0.00")); //mexendo na casa decimal
+
